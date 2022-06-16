859c17e7ff1533b9dff81639e183053ad4770964
Ahua Godwin <57255108+ahuagodwin@users.noreply.github.com>
2022-06-16 14:07:44 +0100

    contact page

--- sendemail.php
+++ sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "youremail@mail.com" );
+define( "RECIPIENT_NAME", "Dozy Mmobuosi Foundation" );
+define( "RECIPIENT_EMAIL", "info@dozymmobuosifoundation.com" );
 
 
 // Read the form values
