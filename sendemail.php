410857a2878769357def8a184679503f6db099cc
Ahua Godwin <57255108+ahuagodwin@users.noreply.github.com>
2022-07-24 16:56:34 +0100

    email script updated

--- sendemail.php
+++ sendemail.php
@@ -7,26 +7,28 @@ define( "RECIPIENT_EMAIL", "info@dozymmobuosifoundation.com" );
 
 // Read the form values
 $success = false;
-$userName = isset( $_POST['username'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
+$senderName = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['name'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['email'] ) : "";
 $senderPhone = isset( $_POST['phone'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['phone'] ) : "";
 $senderSubject = isset( $_POST['subject'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['subject'] ) : "";
+$senderCountry = isset( $_POST['country'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['country'] ) : "";
+$senderState = isset( $_POST['state'] ) ? preg_replace( "/[^\.\-\' a-zA-Z0-9]/", "", $_POST['state'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Message:|Content-Type:)/", "", $_POST['message'] ) : "";
 
 // If all values exist, send the email
-if ( $userName && $senderEmail && $senderPhone && $senderSubject && $message) {
+if ( $senderName && $senderEmail && $senderPhone && $senderSubject && $message && $senderCountry && $senderState) {
   $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
   $headers = "From: " . $name . " <" . $senderEmail . ">";
-  $msgBody = " Phone: " . $senderPhone . " Subject: " . $senderSubject . " Message: " . $message . "";
+  $msgBody = " Phone: " . $senderPhone . " Subject: " . $senderSubject . " Country: " . $senderCountry . " State: " . $senderState . "". "". " Message: " . $message . "";
   $success = mail( $recipient, $headers, $msgBody );
 
   //Set Location After Successsfull Submission
-  header('Location: contact.html?message=Successfull');
+  header('Location: contact.html?message=Message sent Successfull');
 }
 
 else{
 	//Set Location After Unsuccesssfull Submission
-  	header('Location: index.html?message=Failed');	
+  	header('Location: index.html?message=Message Failed');	
 }
 
 ?>
